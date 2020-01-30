fd67b770b74723b3a6b102da9302beb4e90a3f03
captivating-mapping-7374 <mxharris1094@yahoo.com>
2020-01-30 23:53:32 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,16 @@
 # Write a speak_to_grandma method.
+message = gets.chomp
 
+def speak_to_grandma(message)
+   love = "I LOVE YOU GRANDMA!"
+  if message.upcase == love
+    puts "I LOVE YOU TOO PUMPKIN!"
+    
+  elseif message == message.upcase
+    puts "NO, NOT SINCE 1938!"
+    
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
@@ -11,3 +22,4 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+end
